8ec8f8b95de71fd94f067a9f124d22626b951a9c
Chemi <chemialmeida@gmail.com>
2019-09-03 23:32:24 -0400

    Commit inicial

---
+++ multiplicar/multiplicar.js
@@ -0,0 +1,39 @@
+//requireds
+const fs = require('fs');
+const colors = require('colors');
+
+let listarTabla = (base, limite) => {
+
+    console.log('============================'.green)
+    console.log(`========Tabla del ${ base }=========`.green)
+    console.log('============================'.green)
+    for (let i = 1; i <= limite; i++) {
+        console.log(`${base} * ${i} = ${base*i}`);
+    }
+}
+
+crearArchivo = (base, limite) => {
+    return new Promise((resolve, reject) => {
+
+        if (!Number(base)) {
+            reject('El valor introducido no es numerico');
+            return;
+        }
+
+        let data = '';
+
+        for (let i = 1; i < limite; i++) {
+            data += `${base} * ${i} = ${base * i}\n`;
+        }
+
+        fs.writeFile(`tabla-${ base }.txt`, data, (err) => {
+            if (err) reject(err)
+            else resolve(`tabla-${ base }.txt`)
+        });
+    });
+}
+
+module.exports = {
+    crearArchivo,
+    listarTabla
+}
